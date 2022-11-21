ad88beff6bd7e5a3bd353563205ce7be3cbda2b5
Mariano Ardengo <marianoardengo@gmail.com>
2022-11-21 01:11:42 -0300

    Add terraform files to deploy Cloud Run

--- tf/main.tf
+++ tf/main.tf
@@ -4,3 +4,41 @@ resource "google_project_service" "run_api" {
 
   disable_on_destroy = true
 }
+
+resource "google_cloud_run_service" "run_service" {
+  name = "acidlabs-api"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/acidlabs-369203/acidlabs-api:latest"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  # Waits for the Cloud Run API to be enabled
+  depends_on = [google_project_service.run_api]
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.run_service.location
+  project     = google_cloud_run_service.run_service.project
+  service     = google_cloud_run_service.run_service.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
